6261c1d869ae4150b5ee334ee8c639561e093469
Timothy Lindblom <timothy.lindbloma@gmail.com>
2024-04-24 14:11:44 +0200

    Spring cleaning of unused files

--- geoclip/__init__.py
+++
@@ -1,4 +0,0 @@
-from .model import GeoCLIP
-from .model import ImageEncoder
-from .model import LocationEncoder
-from .train import train
--- geoclip/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .GeoCLIP import GeoCLIP
-from .image_encoder import ImageEncoder
-from .location_encoder import LocationEncoder
